cc3c7c61426c84f3414727022254c74c772bc6ac
Ingvar Stepanyan <me@rreverser.com>
2025-08-14 15:49:59 +0100

    Final merge

--- src/convert/closures.rs
+++ src/convert/closures.rs
@@ -1,4 +1,4 @@
-#![allow(clippy::fn_to_numeric_cast)]
+#![allow(clippy::fn_to_numeric_cast, coherence_leak_check)]
 
 use alloc::boxed::Box;
 use core::mem;
@@ -13,8 +13,14 @@ use crate::JsValue;
 use crate::UnwrapThrowExt;
 
 macro_rules! closures {
-    (@impl_for_fn $Fn:ident $FnArgs:tt $FromWasmAbi:ident $is_mut:literal $($mut:ident)? $cnt:literal $($var_expr:expr => $var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) => (const _: () = {
-        #[allow(coherence_leak_check)]
+    (@count_one $ty:ty) => (1);
+
+    (@describe ( $($ty:ty),* $(,)? )) => {
+        inform(0 $(+ closures!(@count_one $ty))*);
+        $(<$ty as WasmDescribe>::describe();)*
+    };
+
+    (@impl_for_fn $is_mut:literal [$($mut:ident)?] $Fn:ident $FnArgs:tt $FromWasmAbi:ident $($var_expr:expr => $var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) => (const _: () = {
         impl<$($var,)* R> IntoWasmAbi for &'_ $($mut)? (dyn $Fn $FnArgs -> R + '_)
             where $($var: $FromWasmAbi,)*
                   R: ReturnWasmAbi
@@ -55,7 +61,6 @@ macro_rules! closures {
             ret.return_abi().into()
         }
 
-        #[allow(coherence_leak_check)]
         impl<$($var,)* R> WasmDescribe for dyn $Fn $FnArgs -> R + '_
             where $($var: $FromWasmAbi,)*
                   R: ReturnWasmAbi
@@ -64,14 +69,12 @@ macro_rules! closures {
             fn describe() {
                 inform(FUNCTION);
                 inform(invoke::<$($var,)* R> as usize as u32);
-                inform($cnt);
-                $(<$var as WasmDescribe>::describe();)*
+                closures!(@describe $FnArgs);
                 <R as WasmDescribe>::describe();
                 <R as WasmDescribe>::describe();
             }
         }
 
-        #[allow(coherence_leak_check)]
         unsafe impl<$($var,)* R> WasmClosure for dyn $Fn $FnArgs -> R + 'static
             where $($var: $FromWasmAbi + 'static,)*
                   R: ReturnWasmAbi + 'static,
@@ -88,9 +91,11 @@ macro_rules! closures {
         }
     };);
 
-    (@impl_for_args $FnArgs:tt $FromWasmAbi:ident $cnt:literal $($var_expr:expr => $var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) => {
-        closures!(@impl_for_fn Fn $FnArgs $FromWasmAbi false $cnt $($var_expr => $var $arg1 $arg2 $arg3 $arg4)*);
-        closures!(@impl_for_fn FnMut $FnArgs $FromWasmAbi true mut $cnt $($var_expr => $var $arg1 $arg2 $arg3 $arg4)*);
+    (@closure ($($ty:ty),* $(,)?) $($var:ident)* $body:block) => (move |$($var: $ty),*| $body);
+
+    (@impl_for_args $FnArgs:tt $FromWasmAbi:ident $($var_expr:expr => $var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) => {
+        closures!(@impl_for_fn false [] Fn $FnArgs $FromWasmAbi $($var_expr => $var $arg1 $arg2 $arg3 $arg4)*);
+        closures!(@impl_for_fn true [mut] FnMut $FnArgs $FromWasmAbi $($var_expr => $var $arg1 $arg2 $arg3 $arg4)*);
 
         // The memory safety here in these implementations below is a bit tricky. We
         // want to be able to drop the `Closure` object from within the invocation of a
@@ -107,12 +112,12 @@ macro_rules! closures {
         // deallocate data immediately.
 
         #[allow(non_snake_case, unused_parens)]
-        impl<T, $($var,)* R> WasmClosureFnOnce<($($var),*), R> for T
-            where T: 'static + FnOnce($($var),*) -> R,
-                $($var: FromWasmAbi + 'static,)*
+        impl<T, $($var,)* R> WasmClosureFnOnce<$FnArgs, R> for T
+            where T: 'static + FnOnce $FnArgs -> R,
+                $($var: $FromWasmAbi + 'static,)*
                 R: ReturnWasmAbi + 'static
         {
-            type FnMut = dyn FnMut($($var),*) -> R;
+            type FnMut = dyn FnMut $FnArgs -> R;
 
             fn into_fn_mut(self) -> Box<Self::FnMut> {
                 let mut me = Some(self);
@@ -131,7 +136,7 @@ macro_rules! closures {
                 let rc1 = Rc::new(WasmRefCell::new(None));
                 let rc2 = rc1.clone();
 
-                let closure = Closure::wrap(Box::new(move |$($var),*| {
+                let closure = Closure::<dyn FnMut $FnArgs -> R>::new(closures!(@closure $FnArgs $($var)* {
                     // Invoke ourself and get the result.
                     let me = me.take().expect_throw("FnOnce called more than once");
                     let result = me($($var),*);
@@ -144,7 +149,7 @@ macro_rules! closures {
                     drop(option_closure);
 
                     result
-                }) as Box<dyn FnMut($($var),*) -> R>);
+                }));
 
                 let js_val = closure.as_ref().clone();
 
@@ -157,21 +162,21 @@ macro_rules! closures {
         }
     };
 
-    ($( ($cnt:literal $($var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) )*) => ($(
-        closures!(@impl_for_args ($($var),*) FromWasmAbi $cnt $($var::from_abi($var) => $var $arg1 $arg2 $arg3 $arg4)*);
+    ($( ($($var:ident $arg1:ident $arg2:ident $arg3:ident $arg4:ident)*) )*) => ($(
+        closures!(@impl_for_args ($($var),*) FromWasmAbi $($var::from_abi($var) => $var $arg1 $arg2 $arg3 $arg4)*);
     )*);
 }
 
 closures! {
-    (0)
-    (1 A a1 a2 a3 a4)
-    (2 A a1 a2 a3 a4 B b1 b2 b3 b4)
-    (3 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4)
-    (4 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4)
-    (5 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4)
-    (6 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4)
-    (7 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4 G g1 g2 g3 g4)
-    (8 A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4 G g1 g2 g3 g4 H h1 h2 h3 h4)
+    ()
+    (A a1 a2 a3 a4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4 G g1 g2 g3 g4)
+    (A a1 a2 a3 a4 B b1 b2 b3 b4 C c1 c2 c3 c4 D d1 d2 d3 d4 E e1 e2 e3 e4 F f1 f2 f3 f4 G g1 g2 g3 g4 H h1 h2 h3 h4)
 }
 
 // Copy the above impls down here for where there's only one argument and it's a
@@ -180,79 +185,4 @@ closures! {
 // just this one! Maybe someone else can figure out voodoo so we don't have to
 // duplicate.
 
-macro_rules! single_ref_closures {
-    ($Fn:ident $is_mut:literal) => {
-        const _: () = {
-            #[allow(coherence_leak_check)]
-            unsafe impl<A, R> WasmClosure for dyn $Fn(&A) -> R
-            where
-                A: RefFromWasmAbi,
-                R: ReturnWasmAbi + 'static,
-            {
-                const IS_MUT: bool = $is_mut;
-            }
-
-            #[allow(coherence_leak_check)]
-            impl<A, R> IntoWasmAbi for &mut (dyn $Fn(&A) -> R + '_)
-            where
-                A: RefFromWasmAbi,
-                R: ReturnWasmAbi,
-            {
-                type Abi = WasmSlice;
-
-                fn into_abi(self) -> WasmSlice {
-                    unsafe {
-                        let (a, b): (usize, usize) = mem::transmute(self);
-                        WasmSlice {
-                            ptr: a as u32,
-                            len: b as u32,
-                        }
-                    }
-                }
-            }
-
-            #[cfg_attr(wasm_bindgen_unstable_test_coverage, coverage(off))]
-            unsafe extern "C" fn invoke1_ref<A: RefFromWasmAbi, R: ReturnWasmAbi>(
-                a: usize,
-                b: usize,
-                arg1: <A::Abi as WasmAbi>::Prim1,
-                arg2: <A::Abi as WasmAbi>::Prim2,
-                arg3: <A::Abi as WasmAbi>::Prim3,
-                arg4: <A::Abi as WasmAbi>::Prim4,
-            ) -> WasmRet<R::Abi> {
-                if a == 0 {
-                    throw_str("closure invoked recursively or after being dropped");
-                }
-                // Scope all local variables before we call `return_abi` to
-                // ensure they're all destroyed as `return_abi` may throw
-                let ret = {
-                    let f: &mut dyn $Fn(&A) -> R = mem::transmute((a, b));
-                    let arg =
-                        <A as RefFromWasmAbi>::ref_from_abi(A::Abi::join(arg1, arg2, arg3, arg4));
-                    f(&*arg)
-                };
-                ret.return_abi().into()
-            }
-
-            #[allow(coherence_leak_check)]
-            impl<A, R> WasmDescribe for dyn $Fn(&A) -> R + '_
-            where
-                A: RefFromWasmAbi,
-                R: ReturnWasmAbi,
-            {
-                #[cfg_attr(wasm_bindgen_unstable_test_coverage, coverage(off))]
-                fn describe() {
-                    inform(FUNCTION);
-                    inform(invoke1_ref::<A, R> as usize as u32);
-                    inform(1);
-                    <&A as WasmDescribe>::describe();
-                    <R as WasmDescribe>::describe();
-                    <R as WasmDescribe>::describe();
-                }
-            }
-        };
-    };
-}
-
-single_ref_closures!(Fn false);
-single_ref_closures!(FnMut true);
+closures!(@impl_for_args (&A,) RefFromWasmAbi &*A::ref_from_abi(A) => A a1 a2 a3 a4);
