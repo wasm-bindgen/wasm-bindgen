ef347c24aaabca55863495d3aa1a4a3ae389cc8f
Ingvar Stepanyan <me@rreverser.com>
2025-09-23 13:34:29 +0100

    Try to support older Node.js

--- examples/nodejs_and_deno/test.js
+++ examples/nodejs_and_deno/test.js
@@ -1,4 +1,6 @@
-const targetName = process.argv[2];
+import { argv } from 'node:process';
+
+const targetName = argv[2];
 
 const { greet } = await import(`../dist/nodejs_and_deno/${targetName}.js`);
 greet(targetName);
