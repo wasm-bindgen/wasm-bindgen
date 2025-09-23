ef40782c81330fa6b0e19daac60e0013b0d5cc8e
Ingvar Stepanyan <me@rreverser.com>
2025-09-23 13:14:01 +0100

    Test more targets in import tests (#4672)
    * Use readable flag suffixes for reference tests
    
    * Test more targets in import tests
    
    import.rs tests a lot more of the module syntax than targets.rs (e.g. raw modules, inline_js, etc) and it will produce different output on different targets, so it's worth testing it as a matrix as well.
    
    * Update CI check

---
+++ crates/cli/tests/reference/import-target-nodejs.js
@@ -0,0 +1,120 @@
+
+let imports = {};
+imports['./snippets/reference-test-ddc0ab9a51c9d25f/inline0.js'] = require('./snippets/reference-test-ddc0ab9a51c9d25f/inline0.js');
+imports['__wbindgen_placeholder__'] = module.exports;
+imports['foo-raw'] = require('foo-raw');
+imports['pure-extern'] = require('pure-extern');
+imports['tests/wasm/imports.js'] = require('tests/wasm/imports.js');
+let wasm;
+const { default: default1 } = require(`tests/wasm/import_class.js`);
+
+function addToExternrefTable0(obj) {
+    const idx = wasm.__externref_table_alloc();
+    wasm.__wbindgen_export_2.set(idx, obj);
+    return idx;
+}
+
+function handleError(f, args) {
+    try {
+        return f.apply(this, args);
+    } catch (e) {
+        const idx = addToExternrefTable0(e);
+        wasm.__wbindgen_exn_store(idx);
+    }
+}
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+function decodeText(ptr, len) {
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
+}
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return decodeText(ptr, len);
+}
+
+function takeFromExternrefTable0(idx) {
+    const value = wasm.__wbindgen_export_2.get(idx);
+    wasm.__externref_table_dealloc(idx);
+    return value;
+}
+
+module.exports.exported = function() {
+    const ret = wasm.exported();
+    if (ret[1]) {
+        throw takeFromExternrefTable0(ret[0]);
+    }
+};
+
+module.exports.__wbg_catchme_f7d87ea824a61e87 = function() { return handleError(function () {
+    catch_me();
+}, arguments) };
+
+module.exports.__wbg_get_56ba567010fb9959 = function(arg0) {
+    const ret = arg0.get();
+    return ret;
+};
+
+module.exports.__wbg_myfunction_8c7b624429f78550 = function() {
+    b.my_function();
+};
+
+module.exports.__wbg_new_d21827b66c7fd25d = function(arg0) {
+    const ret = new default1(arg0);
+    return ret;
+};
+
+module.exports.__wbg_nocatch_be850a8dddd9599d = function() {
+    no_catch();
+};
+
+module.exports.__wbg_reload_84c12f152ad689f0 = function() {
+    window.location.reload();
+};
+
+module.exports.__wbg_static_accessor_CONST_9e9d5ae758197645 = function() {
+    const ret = a.CONST;
+    return ret;
+};
+
+module.exports.__wbg_wbindgenthrow_4c11a24fca429ccf = function(arg0, arg1) {
+    throw new Error(getStringFromWasm0(arg0, arg1));
+};
+
+module.exports.__wbg_write_c2ce0ce33a6087d5 = function(arg0, arg1) {
+    window.document.write(getStringFromWasm0(arg0, arg1));
+};
+
+module.exports.__wbindgen_init_externref_table = function() {
+    const table = wasm.__wbindgen_export_2;
+    const offset = table.grow(4);
+    table.set(0, undefined);
+    table.set(offset + 0, undefined);
+    table.set(offset + 1, null);
+    table.set(offset + 2, true);
+    table.set(offset + 3, false);
+    ;
+};
+
+const path = require('path').join(__dirname, 'reference_test_bg.wasm');
+const bytes = require('fs').readFileSync(path);
+
+const wasmModule = new WebAssembly.Module(bytes);
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+wasm = wasmInstance.exports;
+module.exports.__wasm = wasm;
+
+wasm.__wbindgen_start();
+
