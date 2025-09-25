8ce81b0645f5984216dfe8b8c1cf4a863a3e3b0a
Ingvar Stepanyan <me@rreverser.com>
2025-09-25 21:02:47 +0100

    Add reference test for target.rs + atomics (#4684)
    * Add reference test for target.rs + atomics
    
    The output for atomics is a quite different (it requires importing memory, which is done in various ways on different targets) but so far we haven't had reference tests for it.
    
    * Add nightly toolchain
    
    * Reorder toolchains

---
+++ crates/cli/tests/reference/targets-target-nodejs-atomics.js
@@ -0,0 +1,62 @@
+
+let imports = {};
+imports['__wbindgen_placeholder__'] = module.exports;
+
+let cachedUint8ArrayMemory0 = null;
+
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.buffer !== wasm.memory.buffer) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+function decodeText(ptr, len) {
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().slice(ptr, ptr + len));
+}
+
+function getStringFromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return decodeText(ptr, len);
+}
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+exports.add_that_might_fail = function(a, b) {
+    const ret = wasm.add_that_might_fail(a, b);
+    return ret >>> 0;
+};
+
+exports.__wbg_random_0000000000000000 = function() {
+    const ret = Math.random();
+    return ret;
+};
+
+exports.__wbg_wbindgenthrow_0000000000000001 = function(arg0, arg1) {
+    throw new Error(getStringFromWasm0(arg0, arg1));
+};
+
+exports.__wbindgen_init_externref_table = function() {
+    const table = wasm.__wbindgen_export_1;
+    const offset = table.grow(4);
+    table.set(0, undefined);
+    table.set(offset + 0, undefined);
+    table.set(offset + 1, null);
+    table.set(offset + 2, true);
+    table.set(offset + 3, false);
+    ;
+};
+imports.wbg = { memory: new WebAssembly.Memory({initial:18,maximum:16384,shared:true}) };
+const wasmPath = `${__dirname}/reference_test_bg.wasm`;
+const wasmBytes = require('fs').readFileSync(wasmPath);
+const wasmModule = new WebAssembly.Module(wasmBytes);
+const wasm = exports.__wasm = new WebAssembly.Instance(wasmModule, imports).exports;
+
+wasm.__wbindgen_start();
+
