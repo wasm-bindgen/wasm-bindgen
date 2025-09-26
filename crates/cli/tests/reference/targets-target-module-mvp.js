639b76a406389d7f47646b7ea7c6619b892ee573
daxpedda <daxpedda@gmail.com>
2025-09-26 14:49:24 +0200

    Add reference tests for MVP

---
+++ crates/cli/tests/reference/targets-target-module-mvp.js
@@ -0,0 +1,26 @@
+import source wasmModule from "./reference_test_bg.wasm";
+
+let wasm;
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export function add_that_might_fail(a, b) {
+    const ret = wasm.add_that_might_fail(a, b);
+    return ret >>> 0;
+}
+
+const imports = {
+    __wbindgen_placeholder__: {
+        __wbg_random_9526caf33df4270d: function() {
+            const ret = Math.random();
+            return ret;
+        },
+    },
+
+};
+
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+wasm = wasmInstance.exports;
+
