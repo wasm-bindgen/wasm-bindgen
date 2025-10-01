75d3c43b57e2a6f67af41c384326cd1de3975248
Ingvar Stepanyan <me@rreverser.com>
2025-10-01 18:24:43 +0100

    Add integration test for experimental-nodejs-module (#4695)
    We can't use wasm-pack here like we do for all other tests, but at least with Cargo's message-format=json the custom script is pretty trivial.

--- examples/nodejs_and_deno/test.js
+++ examples/nodejs_and_deno/test.js
@@ -2,5 +2,5 @@ import { argv } from 'node:process';
 
 const targetName = argv[2];
 
-const { greet } = await import(`../dist/nodejs_and_deno/${targetName}.js`);
+const { greet } = await import(`../dist/nodejs_and_deno/${targetName}/node_and_deno.js`);
 greet(targetName);
