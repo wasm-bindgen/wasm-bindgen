8cbb857bdb0e0ebc72344b1cb27c998e9a71cf48
Spxg <unsafe@outlook.es>
2025-10-09 12:54:42 +0800

    test ci

--- examples/wasm-audio-worklet/src/lib.rs
+++ examples/wasm-audio-worklet/src/lib.rs
@@ -10,6 +10,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub async fn web_main() {
+    console_error_panic_hook::set_once();
     // On the application level, audio worklet internals are abstracted by wasm_audio:
     let params: &'static Params = Box::leak(Box::default());
     let mut osc = Oscillator::new(params);
