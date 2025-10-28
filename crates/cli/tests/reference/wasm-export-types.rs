ef0a3451cdcc9b68adef03e5039a98e4abf74375
Lynn <lynn@foldr.moe>
2025-10-28 16:46:56 +0100

    Undo debugging change

--- crates/cli/tests/reference/wasm-export-types.rs
+++ crates/cli/tests/reference/wasm-export-types.rs
@@ -1,8 +1,6 @@
 // FLAGS: --target=web
-// DEPENDENCY: wasm-bindgen-test = { path = '{root}/crates/test' }
 
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_test::*;
 
 // This is for testing the type generation of the wasm-exported functions.
 // Here, example should be exported as `(arg0: number, arg1: bigint, arg2: any, arg3: number, arg4: number) => [number, number]`.
@@ -16,9 +14,4 @@ pub fn example(a: u32, b: u64, c: JsValue, d: &str) -> String {
 #[wasm_bindgen]
 pub fn example_128(a: u128) -> Option<u128> {
     None
-}
-
-#[wasm_bindgen_test]
-fn example_test() {
-    assert_eq!(example_128(1), None)
 }
