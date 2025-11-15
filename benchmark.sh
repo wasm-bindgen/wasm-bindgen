bc032330e80f6128d735cdb295a7eec0135fbc02
Spxg <unsafe@outlook.es>
2025-11-15 10:53:03 +0800

    test

--- benchmark.sh
+++ benchmark.sh
@@ -2,6 +2,8 @@
 
 export WASM_BINDGEN_BENCH_RESULT=$(pwd)/target/wbg_bench.json
 export WASM_BINGEN_CODSPEED_NAPI=$(pwd)/index.node
+export NODE_ARGS="--perf-basic-prof-only-functions"
+export CODSPEED_LOG="debug"
 
 cargo bench --target wasm32-unknown-unknown -p js-sys
 
