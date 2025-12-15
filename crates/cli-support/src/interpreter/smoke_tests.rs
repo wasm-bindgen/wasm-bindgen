48633b604cf24ca51b9b4bc11e7ff58ea17ef4e9
Bouke van der Bijl <bouke@monumental.co>
2025-12-15 10:00:26 +0100

    interpreter: support try_table blocks (#4862)
    * interpreter: support try_table blocks
    
    Fixes #4861
    
    * Remove unneeded tests
    
    Copy paste from the 'smoke' test
    
    * Add changelog entry

--- crates/cli-support/src/interpreter/smoke_tests.rs
+++ crates/cli-support/src/interpreter/smoke_tests.rs
@@ -242,15 +242,6 @@ fn calling_functions() {
 fn try_block() {
     let wat = r#"
         (module
-            (export "foo" (func $foo))
-
-            (func $foo)
-        )
-    "#;
-    interpret(wat, "foo", &[]);
-
-    let wat = r#"
-        (module
             (import "__wbindgen_placeholder__" "__wbindgen_describe"
               (func $__wbindgen_describe (param i32)))
             (global (mut i32) (i32.const 0))
@@ -272,6 +263,46 @@ fn try_block() {
                     call $__wbindgen_describe
                 catch_all
                 end
+
+                ;; increment our stack pointer
+                local.get 0
+                i32.const 16
+                i32.add
+                global.set 0
+            )
+
+            (export "foo" (func $foo))
+        )
+    "#;
+    interpret(wat, "foo", &[1]);
+}
+
+#[test]
+fn try_table_block() {
+    let wat = r#"
+        (module
+            (import "__wbindgen_placeholder__" "__wbindgen_describe"
+              (func $__wbindgen_describe (param i32)))
+            (global (mut i32) (i32.const 0))
+
+            (func $foo
+                (local i32)
+
+                ;; decrement the stack pointer, setting our local to the
+                ;; lowest address of our stack
+                global.get 0
+                i32.const 16
+                i32.sub
+                local.set 0
+                local.get 0
+                global.set 0
+
+                (block $catch
+                    (try_table (catch_all $catch)
+                        i32.const 1
+                        call $__wbindgen_describe
+                    )
+                )
 
                 ;; increment our stack pointer
                 local.get 0
