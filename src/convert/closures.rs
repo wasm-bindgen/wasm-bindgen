e99742a73adbab9ff9e998efe83d96be0de2df60
Guy Bedford <gbedford@cloudflare.com>
2025-12-19 05:01:08 -0800

    fixup

--- src/convert/closures.rs
+++ src/convert/closures.rs
@@ -2,9 +2,10 @@ use alloc::boxed::Box;
 use core::mem;
 
 #[cfg(all(feature = "catch-unwind", panic = "unwind"))]
+use crate::__rt::maybe_catch_unwind;
+#[cfg(all(feature = "catch-unwind", panic = "unwind"))]
 use std::panic::AssertUnwindSafe;
 
-use crate::__rt::maybe_catch_unwind;
 use crate::closure::{Closure, IntoWasmClosure, WasmClosure, WasmClosureFnOnce};
 use crate::convert::slices::WasmSlice;
 use crate::convert::RefFromWasmAbi;
@@ -67,9 +68,14 @@ macro_rules! closures {
                 $(
                     let $var = $var::Abi::join($arg1, $arg2, $arg3, $arg4);
                 )*
-                if cfg!(all(feature = "catch-unwind", panic = "unwind")) {
+
+                #[cfg(all(feature = "catch-unwind", panic = "unwind"))]
+                {
                     maybe_catch_unwind(AssertUnwindSafe(|| f($($var_expr),*)))
-                } else {
+                }
+
+                #[cfg(not(all(feature = "catch-unwind", panic = "unwind")))]
+                {
                     f($($var_expr),*)
                 }
             };
