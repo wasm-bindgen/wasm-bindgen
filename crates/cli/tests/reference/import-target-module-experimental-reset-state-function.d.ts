359f444ca2c0e9d1a99e6efb0d8c4f5dc09048b7
Logan Gatlin <lgatlin@cloudflare.com>
2026-03-12 10:58:06 -0500

    Fix missing type error

--- crates/cli/tests/reference/import-target-module-experimental-reset-state-function.d.ts
+++ crates/cli/tests/reference/import-target-module-experimental-reset-state-function.d.ts
@@ -3,6 +3,6 @@
 
 export function __wbg_reset_state(): void;
 
-export function __wbg_set_reinit_hook(callback: ((newInstance: InitOutput, oldInstance: InitOutput) => void) | null): void;
+export function __wbg_set_reinit_hook(callback: ((newInstance: WebAssembly.Exports, oldInstance: WebAssembly.Exports) => void) | null): void;
 
 export function exported(): void;
