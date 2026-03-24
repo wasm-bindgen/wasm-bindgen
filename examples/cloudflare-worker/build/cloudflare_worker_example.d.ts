f64a12785e06b1265a931bbc93c10acf1241b163
Guy Bedford <gbedford@cloudflare.com>
2026-03-24 14:35:44 -0700

    remove accidental cloudflare-worker build artifacts

--- examples/cloudflare-worker/build/cloudflare_worker_example.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-
-declare function fetch(_req: any, env: any, _ctx: any): Promise<any>;
-
-declare let _default: {
-    fetch: typeof fetch,
-};
-export default _default;
